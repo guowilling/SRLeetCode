5cbb2ba8f4c34f3478934bcba5721f6a930688a5
Willing Guo <guowilling90@gmail.com>
2018-12-27 17:30:01 +0800

    Move Zeros

---
+++ Playgrounds/Move Zeros.playground/Contents.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+// 283题: Move Zeros
+
+// 给定一个数组 nums，写一个函数，将数组中所有的0挪到数组的末尾，⽽维持其他所有非0元素的相对位置。
+// 举例: nums = [0, 1, 0, 3, 12]，函数运⾏后结果为[1, 3, 12, 0, 0]
+
+// 解法一:
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+func moveZeros1(nums: [Int]) -> [Int] {
+    var nonZeroElements: [Int] = [Int]()
+    for num in nums {
+        if num != 0 {
+            nonZeroElements.append(num)
+        }
+    }
+    for _ in 0..<(nums.count - nonZeroElements.count) {
+        nonZeroElements[0] = nonZeroElements[1]
+        nonZeroElements.append(0)
+    }
+    return nonZeroElements
+}
+var nums1 = [0, 1, 0, 3, 12]
+print(moveZeros1(nums: nums1))
+
+// 解法二
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+func moveZeros2(nums: inout [Int]) -> [Int] {
+    var k = 0
+    for num in nums {
+        if num != 0 {
+            nums[k] = num
+            k += 1
+        }
+    }
+    for i in k..<nums.count {
+        nums[i] = 0
+    }
+    return nums
+}
+var nums2 = [0, 1, 0, 3, 12]
+print(moveZeros2(nums: &nums2))
+
+// 解法三
+// 思路：非零元素与之前的零元素进行交换
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+func moveZeros3(nums: inout [Int]) -> [Int] {
+    var k = 0
+    for (i, num) in nums.enumerated() {
+        if num != 0 {
+            if k != i {
+                let temp = nums[i]
+                nums[i] = nums[k]
+                nums[k] = temp
+                k += 1
+            } else {
+                k += 1
+            }
+        }
+    }
+    return nums
+}
+var nums3 = [0, 1, 0, 3, 12]
+print(moveZeros3(nums: &nums3))
