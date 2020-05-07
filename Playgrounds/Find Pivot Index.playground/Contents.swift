9172cf6a36446ed5fb9de88a952ebd182aa53d52
Willing Guo <guowilling90@gmail.com>
2020-05-07 14:06:11 +0800

    Find Pivot Index

---
+++ Playgrounds/Find Pivot Index.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+// 724. 寻找数组的中心索引
+
+// 链接: https://leetcode-cn.com/problems/find-pivot-index/
+// 要点: 两次遍历，一次求总和，一次渐进求和判断
+// 时间复杂度(n)
+// 空间复杂度(1)
+
+class Solution {
+    func pivotIndex(_ nums: [Int]) -> Int {
+        let sum = nums.reduce(0, +)
+        var current = 0
+        for (index, num) in nums.enumerated() {
+            if sum - num - current == current {
+                return index
+            }
+            current += num
+        }
+        return -1
+    }
+}
+
+let s = Solution()
+assert(s.pivotIndex([1, 7, 3, 6, 5, 6]) == 3)
+assert(s.pivotIndex([1, 2, 3]) == -1)
