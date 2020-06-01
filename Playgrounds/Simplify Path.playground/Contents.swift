8448094e71acccfe5d94727bf7062846ea412863
Willing Guo <guowiiling90@gmail.com>
2020-06-01 22:16:47 +0800

    Simplify Path

---
+++ Playgrounds/Simplify Path.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+// 71. 简化路径
+// 时间复杂度：O(N)
+// 空间复杂度：O(N)
+
+class Solution {
+    func simplifyPath(_ path: String) -> String {
+        let paths = path.split(separator: "/")
+        var pathStack: [String] = []
+        for path in paths {
+            guard path != ".", !path.isEmpty else { continue }
+            if path == ".." {
+                pathStack.popLast()
+            } else {
+                pathStack.append(String(path))
+            }
+        }
+        return "/" + pathStack.joined(separator: "/")
+    }
+}
+
+let s = Solution()
+s.simplifyPath("/home/") == "/home"
+s.simplifyPath("/../") == "/"
+s.simplifyPath("/home//foo/") == "/home/foo"
+s.simplifyPath("/a/./b/../../c/") == "/c"
+s.simplifyPath("/a/../../b/../c//.//") == "/c"
+s.simplifyPath("/a//b////c/d//././/..") == "/a/b/c"
