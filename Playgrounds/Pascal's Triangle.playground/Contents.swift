0041a33d8480719572d6f394ba73dd1a24a377d9
Willing Guo <guowilling90@gmail.com>
2020-11-17 00:29:25 +0800

    Update

--- Playgrounds/Pascal's Triangle.playground/Contents.swift
+++ Playgrounds/Pascal's Triangle.playground/Contents.swift
@@ -1,12 +1,24 @@
-import UIKit
-
+//
 // 118. 杨辉三角
+//
+// https://leetcode-cn.com/problems/pascals-triangle/
+//
+// 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+// 在杨辉三角中，每个数是它左上方和右上方的数的和。
+//
+// 示例:
+// 输入: 5
+// 输出:
+// [
+//      [1],
+//     [1,1],
+//    [1,2,1],
+//   [1,3,3,1],
+//  [1,4,6,4,1]
+// ]
+//
 
-// 链接: https://leetcode-cn.com/problems/pascals-triangle/
-// 要点：根据杨辉三角规律迭代生成
-// 时间复杂度：O(n^2)
-// 空间复杂度：O(n^2)
-
+/// O(n²), O(n²)
 class Solution {
     func generate(_ nowRows: Int) -> [[Int]] {
         var triangle: [[Int]] = []
@@ -22,6 +34,7 @@ class Solution {
             row.append(1)
             triangle.append(row)
         }
+        
         return triangle
     }
 }
